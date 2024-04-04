1436635bdef2c98bd30069fc7653210ca4480ca6
Johanna Keïta <keitajohanna@gmail.com>
2024-04-04 19:24:32 +0200

    PlantItems et props CareScale style

--- src/Components/CareScale.js
+++ src/Components/CareScale.js
@@ -1,13 +1,13 @@
 import Sun from '../Assets/Sun.png'
 import Water from'../Assets/Water.png'
-
+import '../Styles/PlantItem.css'
 function CareScale({ scaleValue, careType }) {
 	const range = [1, 2, 3]
 	const scaleType =
 		careType === 'light' ? (
-			<img src={Sun} alt='sun-icon' />
+			<img src={Sun} alt='sun-icon' className='lmj-care-type'/>
 		) : (
-			<img src={Water} alt='water-icon' />
+			<img src={Water} alt='water-icon' className='lmj-care-type'/>
 		)
 
 	return (
